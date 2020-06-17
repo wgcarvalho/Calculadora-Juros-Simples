1985028e58ff299f05cfd5b323967fbb061c3125
wgcarvalho <williangcarv@gmail.com>
2020-06-17 19:35:54 -0300

    Inclusão de validador nos campos TextField

--- lib/main.dart
+++ lib/main.dart
@@ -12,57 +12,85 @@ class JS extends StatefulWidget {
 }
 
 class _JSState extends State<JS> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>(); //1º criar a variavel GlobalKey
+
   TextEditingController capital = new TextEditingController();
   TextEditingController taxa = new TextEditingController();
   TextEditingController mes = new TextEditingController();
 
   String resultado = "Resultado:";
 
-  void calcular(){
+  void calcular() {
     double capitalN = double.parse(capital.text);
-    double taxaN = double.parse(taxa.text)/100;
+    double taxaN = double.parse(taxa.text) / 100;
     double mesN = double.parse(mes.text);
 
-    double montante = capitalN + (capitalN*taxaN*mesN);
+    double montante = capitalN + (capitalN * taxaN * mesN);
 
     setState(() {
-      resultado = "O montante final será de ${montante.toStringAsPrecision(7)} reais ";
+      resultado =
+          "O montante final será de ${montante.toStringAsPrecision(7)} reais ";
     });
-
   }
 
+  String validacao(String value) {
+    if (value.isEmpty) {
+      return "Insira o valor solicitado";
+    } else {
+      if (double.parse(value) <= 0) {
+        return "o valor deve ser maior que zero";
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calcula de Juros Simples"),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: <Widget>[
-          TextFormField(
-            controller: capital,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(labelText: 'Insira o Capital Inicial:'),
+        appBar: AppBar(
+          title: Text("Calcula de Juros Simples"),
+          centerTitle: true,
+        ),
+        body: Form(//2º cria o widget
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: capital,
+                keyboardType: TextInputType.number,
+                decoration:
+                    InputDecoration(labelText: 'Insira o Capital Inicial:'),
+                validator: (value) {
+                  return validacao(value);
+                },
+              ),
+              TextFormField(
+                controller: taxa,
+                keyboardType: TextInputType.number,
+                decoration:
+                    InputDecoration(labelText: 'Insira a Taxa de Juros (mês):'),
+                validator: (value) {
+                  return validacao(value);
+                },
+              ),
+              TextFormField(
+                controller: mes,
+                keyboardType: TextInputType.number,
+                decoration:
+                    InputDecoration(labelText: 'Insira o periodo (meses):'),
+                validator: (value) {
+                  return validacao(value);
+                },
+              ),
+              RaisedButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      calcular();
+                    }
+                  },
+                  child: Text("Calcular")),
+              Text(resultado),
+            ],
           ),
-          TextFormField(
-            controller: taxa,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(labelText: 'Insira a Taxa de Juros (mês):'),
-          ),
-          TextFormField(
-            controller: mes,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(labelText: 'Insira o periodo (meses):'),
-          ),
-          RaisedButton(
-            onPressed: (){calcular();},
-            child: Text("Calcular")
-          ),
-          Text(resultado),
-        ],
-      ),
-    );
+        ));
   }
 }
